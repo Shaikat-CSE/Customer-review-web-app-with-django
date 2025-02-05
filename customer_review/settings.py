3420e5ccdc9e1dc878032b90898986dbcc290e99
Shaikat S. <shaikat143@gmail.com>
2025-02-06 02:21:16 +0600

    Update the app with analytics and other improvements

--- customer_review/settings.py
+++ customer_review/settings.py
@@ -71,6 +71,7 @@ TEMPLATES = [
 ]
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 WSGI_APPLICATION = 'customer_review.wsgi.application'
